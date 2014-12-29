08e0a995c84f22e97a937ccce1284a3ee7bc20a7
regonn <regonn314@gmail.com>
2014-12-30 02:31:51 +0900

    add cat

---
+++ cat.rs
@@ -0,0 +1,32 @@
+#![allow(unused_must_use)]
+use std::os;
+use std::io::{File};
+use std::io::stdio::{stdout_raw, stderr};
+
+fn main() {
+    let mut args = os::args();
+    args.remove(0);
+    let mut stderr = stderr();
+
+    if args.len() < 1 {
+        stderr.write_str("{}: file name not given\n");
+    }
+    for path in args.iter() {
+        do_cat(path);
+    }
+}
+
+fn do_cat(path: &String) {
+    let mut writer = stdout_raw();
+    let mut in_buf = [0, .. 1024];
+    let mut reader = File::open(&std::path::Path::new(path));
+
+    loop {
+        match reader.read(&mut in_buf) {
+            Ok(n) if n != 0 => {
+                writer.write(in_buf.slice_to(n)).unwrap();
+            },
+            _ => break
+        }
+    }
+}
