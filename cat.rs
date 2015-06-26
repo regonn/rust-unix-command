a9876740f13855b30d2d466659d6d140505658ac
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2015-06-26 12:20:48 +0900

    brush up

--- cat.rs
+++ cat.rs
@@ -1,27 +1,26 @@
 use std::fs::File;
 use std::io::{BufReader, Read, Write};
 use std::io::stdout;
+use std::env::args;
 
 const BUFFER_SIZE: usize = 2048;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let paths = &args[1..];
-
-    if paths.len() < 1 {
+    let paths: Vec<String> = args().skip(1).collect();
+    if paths.is_empty() {
         panic!("file name not given");
     }
+
+    let mut writer = stdout();
     for path in paths {
-        do_cat(&path);
+        do_cat(&mut writer, &path);
     }
 }
 
-fn do_cat(path: &str) {
+fn do_cat(writer: &mut Write, path: &str) {
     let file = File::open(path).unwrap();
     let mut reader = BufReader::new(&file);
-
     let mut buf = [0; BUFFER_SIZE];
-    let mut writer = stdout();
     loop {
         let n = reader.read(&mut buf).unwrap();
         if n == 0 { break; }
